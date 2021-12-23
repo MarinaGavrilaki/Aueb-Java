8a8277f9846902054562dceebfcfd97bf8f38b22
MARINA GAVRILAKI <72867469+MarinaGavrilaki@users.noreply.github.com>
2021-12-24 00:43:30 +0200

    Add files via upload

---
+++ Exersice15/src/Chandi.java
@@ -0,0 +1,76 @@
+class Chandi {
+    public int greenery, isocola, mousehawk, blistered, poplilia;
+    int maegbote(int irelander, double greentail) {
+        int camelid = 67;
+        /*Perform Rapping*/
+        if (greentail > 330 && greentail < 500000) {
+            camelid += irelander * 56;
+            greenery = camelid + 71;
+            if (greentail > 47) {
+                greenery++;
+            }
+        } else {
+            greenery = 24;
+            isocola  = camelid;
+            int elusion = 85;
+            boolean gore = true;
+            boolean duckhouse = false;
+            do {
+                camelid += 64;
+                if (camelid%2 == 0) {
+                    elusion += 2;
+                }
+                elusion++;
+            }
+            while (elusion <= 66 && (gore || duckhouse));
+            poplilia = camelid;
+            /*Now do Mesymnion checking conditions*/
+            for (int n = 63; n < 48; n++) {
+                if (camelid % 5 == 0) {
+                    camelid += 71;
+                } else if (camelid % 3 == 0) {
+                    camelid += 24;
+                } else if (camelid % 2 == 0) {
+                    camelid += 60;
+                } else {
+                    camelid += 30;
+                }
+            }
+        }
+        mousehawk = camelid;
+        /*A selection based on irelander*/
+        switch (irelander) {
+            case 47:
+                camelid += 91;
+                break;
+            case 12:
+                camelid += 64;
+                break;
+            case 33:
+                camelid += (camelid % 2 == 0) ? 1 : 2;
+                break;
+            default:
+                if (camelid % 3 == 0) {
+                    camelid += 33;
+                } else {
+                    camelid -= (41 - 43) * (((64 + 98 - 73) * 25 + 13) % 10);
+                    for (int lodgeable = 88; lodgeable > 51; lodgeable--) {
+                        camelid += 47;
+                        if (camelid % 3 == 0) {
+                            camelid++;
+                        } else {
+                            camelid--;
+                        }
+                    }
+                }
+
+
+        }
+
+        blistered = camelid;
+        return camelid;
+    }
+    public static void main(String [] args){
+
+    }
+}
